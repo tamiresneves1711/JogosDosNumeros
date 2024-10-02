c80cd5a943f6e092ae6fdce6de1fc57b0962b3a9
tamiresneves1711 <tamiresneves1711@gmail.com>
2024-10-02 16:30:20 -0300

    criando função

--- script.js
+++ script.js
@@ -10,3 +10,26 @@ function gerarNumeroAleatorio(){
         }, 2000);
     });
 }
+
+function verificarPalpite(palpite){
+    return new Promise((resolve, reject) => {
+        tentativas++;
+        if(palpite < 1 || palpite >100){
+            reject("Por Favor, digite um número entre 1 e 100");
+        }else if(palpite === numerosecreto){
+            resolve("Parabéns! Você acertou o número");
+        }else if(palpite < numerosecreto){
+            resolve("Muito baixo! Jogue novamente")
+        }else{
+            resolve("Muito alto! Jogue novamente")
+        }
+    });
+}
+
+async function iniciarjogo(){
+    await gerarNumeroAleatorio();
+    const botaoEnviar = document.getElementById("enviar");
+    const inputPalpite = document.getElementById("palpite");
+    const resultado = document.getElementById("resultado");
+    const tentativasDisplay = document.getElementById("tentativas")
+}
